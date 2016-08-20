989e1789f9d8622c599a3085602c85e01855c401
Anatoliy Musiyuk <anatoliy.musiyuk@gmail.com>
2016-08-20 12:26:12 +0300

    Test2

---
+++ app/Http/Requests/TestValidation.php
@@ -0,0 +1,20 @@
+<?php namespace App\Http\Requests;
+use App\Http\Requests\Request;
+
+class TestValidation extends Request {
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|unique|max:255',
+            'body' => 'required',
+        ];
+    }
+
+    
+}
